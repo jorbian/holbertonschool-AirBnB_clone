770a70e7189f6968609b115ffd374b37ec825f33
jorbian <60049930+jorbian@users.noreply.github.com>
2023-02-07 16:47:33 -0600

    another tweek to see if I can fix whats wrong

--- console.py
+++ console.py
@@ -5,7 +5,11 @@ import cmd, subprocess, re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
-
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     intro = ''
